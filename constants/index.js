7e681985c69a4e0e533ecf8cc7381a16639d3003
esibrahimi336 <141681861+esibrahimi336@users.noreply.github.com>
2023-08-08 00:47:40 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1c5b372639d4bacd0c0a9af586f59ef4b9c273e4";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
